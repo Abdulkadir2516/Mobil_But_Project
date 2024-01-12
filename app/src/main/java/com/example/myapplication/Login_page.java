37e46ea935171d7ccb0008b31ae56c0bc17b8a74
Abdulkadir Yeşilkaya <abdulkadir7725@gmail.com>
2024-01-13 01:50:43 +0300

    Splash bitti Log In ve Sign Up sayfaların tasarımı bitti sayfalar arası geçiş yapıldı

--- app/src/main/java/com/example/myapplication/Login_page.java
+++ app/src/main/java/com/example/myapplication/Login_page.java
@@ -2,7 +2,10 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login_page extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class Login_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button sign_up = findViewById(R.id.sign_up2);
+
+        sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sign_page = new Intent(Login_page.this, Signup_page.class);
+                startActivity(sign_page);
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/myapplication/Signup_page.java
+++ app/src/main/java/com/example/myapplication/Signup_page.java
@@ -2,7 +2,10 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Signup_page extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class Signup_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        Button login = findViewById(R.id.log_in);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent login_page = new Intent(Signup_page.this, Login_page.class);
+                startActivity(login_page);
+            }
+        });
+
     }
 }
